25ec7e50c62b34005852deef96bf74550221fdd6
AntonKaravaev <karavaevanton1993@gmail.com>
2021-03-27 15:20:12 +0300

    Добавил класс TravelMapItem

--- src/main/java/com/summerdev/travel/domain/TravelMapDomainImpl.java
+++ src/main/java/com/summerdev/travel/domain/TravelMapDomainImpl.java
@@ -4,8 +4,10 @@ import com.summerdev.travel.requests.TravelMapRequest;
 import com.summerdev.travel.responses.TravelMapResponse;
 
 public class TravelMapDomainImpl implements TravelMapDomain {
+
     @Override
     public TravelMapResponse getTravelMap(TravelMapRequest request) {
         return null;
+
     }
 }
--- src/main/java/com/summerdev/travel/responses/TravelMapResponse.java
+++ src/main/java/com/summerdev/travel/responses/TravelMapResponse.java
@@ -1,4 +1,7 @@
 package com.summerdev.travel.responses;
 
+import java.util.ArrayList;
+
 public class TravelMapResponse {
+    ArrayList<TravelMapItem> arrayList;
 }
